b70438f9192e968fc3b12f44cc32bcb1bf3e29b7
Muhammed Batuhan Aydın <56546166+Batuhanaydn@users.noreply.github.com>
2020-11-26 00:54:29 +0300

    wiki-scraper-v1

---
+++ scraper.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+import random
+def scrapeWikiArticle(url):
+    response = requests.get(
+        url=url,
+    )
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    title = soup.find(id="firstHeading")
+    print(title.string)
+
+    allLinks = soup.find(id="bodyContent").find_all("a")
+    random.shuffle(allLinks)
+    linkToScrape = 0
+
+    for link in allLinks:
+        if link["href"].find("/wiki/") == -1:
+            continue
+
+        linkToScrape = link
+        break
+    scrapeWikiArticle("https://tr.wikipedia.org/"+ linkToScrape["href"])
+
+scrapeWikiArticle("https://tr.wikipedia.org/wiki/Python_(programlama_dili)")
